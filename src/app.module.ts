6f6c7b4c81940ad8bbe713072237fd90e9e1b4e2
Jenish Bhilavala <jenishbhilavala.shivinfotech@gmail.com>
2024-12-16 13:11:37 +0530

    Initial-setup done

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const config: any = {
+  dialect: 'mysql',
+  autoLoadModels: true,
+  models: [],
+  define: {
+    timestamps: false,
+  },
+};
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      ...config,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,61 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
+import 'winston-daily-rotate-file';
+import * as compression from 'compression';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.File({
+          filename: `logs/error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        new transports.File({
+          filename: `logs/combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            })
+          ),
+        }),
+      ],
+    }),
+  });
+
+  app.enableCors();
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('cuentista')
+    .setDescription('Cuentista project using NestJS')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+
+  app.use(compression());
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  const port = process.env.APP_PORT || 3333;
+  await app.listen(port);
+  Logger.log(`Server is listening on port ${port} `);
+}
+bootstrap();
