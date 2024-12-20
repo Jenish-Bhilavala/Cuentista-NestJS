62a0bbb67e125e469d367c4f68504f00ab5ca5ef
Jenish Bhilavala <jenishbhilavala.shivinfotech@gmail.com>
2024-12-20 13:19:47 +0530

    Add product done

---
+++ src/model/product.model.ts
@@ -0,0 +1,62 @@
+import { MaxLength } from 'class-validator';
+import { DataTypes } from 'sequelize';
+import {
+  AllowNull,
+  Column,
+  Default,
+  HasMany,
+  Model,
+  Sequelize,
+  Table,
+} from 'sequelize-typescript';
+import { ProductBenefitModel } from './product_benefit.model';
+import { ImageModel } from './images.model';
+import { ProductExpertiseModel } from './product_expertise.model';
+import { ProductMethodologyModel } from './product_methodology.model';
+import { ProductServiceModel } from './product_service.model';
+
+@Table({ tableName: 'product' })
+export class ProductModel extends Model<ProductModel> {
+  @AllowNull(false)
+  @MaxLength(50)
+  @Column
+  product_name: string;
+
+  @AllowNull(false)
+  @MaxLength(255)
+  @Column
+  product_description: string;
+
+  @AllowNull(false)
+  @MaxLength(255)
+  @Column
+  contact: string;
+
+  @HasMany(() => ProductBenefitModel)
+  benefit: ProductBenefitModel[];
+
+  @HasMany(() => ProductServiceModel)
+  services: ProductServiceModel[];
+
+  @HasMany(() => ProductMethodologyModel)
+  methodologies: ProductMethodologyModel[];
+
+  @HasMany(() => ProductExpertiseModel)
+  expertise: ProductExpertiseModel[];
+
+  @HasMany(() => ImageModel)
+  imagesDetails: ImageModel[];
+
+  @Default(Sequelize.literal('CURRENT_TIMESTAMP'))
+  @Column({ type: 'TIMESTAMP' })
+  createdAt: Date;
+
+  @Default(Sequelize.literal('CURRENT_TIMESTAMP'))
+  @Column({
+    type: 'TIMESTAMP',
+    defaultValue: Sequelize.literal(
+      'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+    ),
+  })
+  updatedAt: Date;
+}
---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,106 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  MaxLength,
+  IsString,
+  IsOptional,
+  IsArray,
+} from 'class-validator';
+
+export class CreateProductDTO {
+  @ApiProperty({
+    description: 'The name of the product',
+    maxLength: 50,
+  })
+  @IsNotEmpty()
+  @MaxLength(50)
+  product_name: string;
+
+  @ApiProperty({
+    description: 'The description of the product',
+    maxLength: 255,
+  })
+  @IsNotEmpty()
+  @MaxLength(255)
+  product_description: string;
+
+  @ApiProperty({
+    description: 'Contact details for the product',
+    maxLength: 255,
+  })
+  @IsNotEmpty()
+  @MaxLength(255)
+  contact: string;
+
+  @ApiProperty({
+    description: 'Image details for the product',
+    type: Object,
+    required: false,
+    example: {
+      image1: 'image_url_1',
+      image2: 'image_url_2',
+      image3: 'image_url_3',
+      image4: 'image_url_4',
+    },
+  })
+  @IsNotEmpty()
+  images: {
+    image1: string;
+    image2: string;
+    image3: string;
+    image4: string;
+  };
+
+  @ApiProperty({
+    description: 'List of benefits related to the product',
+    type: [String],
+  })
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  benefits: string[];
+
+  @ApiProperty({
+    description: 'List of services offered with the product',
+    type: [Object],
+    example: [
+      {
+        service_type: 'Service Type 1',
+        service_details: 'Detailed description of the service',
+        service_details_list: ['sub description 1', 'sub description 2'],
+      },
+    ],
+  })
+  @IsArray()
+  @IsOptional()
+  services: {
+    service_type: string;
+    service_details: string;
+    service_details_list: string[];
+  }[];
+
+  @ApiProperty({
+    description: 'List of methodologies associated with the product',
+    type: [String],
+  })
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  methodology: string[];
+
+  @ApiProperty({
+    description: 'List of expiry areas and descriptions',
+    type: [Object],
+    required: false,
+    example: [
+      { area: 'Area 1', description: 'Expiry description 1' },
+      { area: 'Area 2', description: 'Expiry description 2' },
+    ],
+  })
+  @IsArray()
+  @IsOptional()
+  expertise: {
+    area: string;
+    description: string;
+  }[];
+}
